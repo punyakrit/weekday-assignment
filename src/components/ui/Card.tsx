9156edd91945f4de5077319ffbc60b48b8bc372e
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-05-02 03:58:02 +0530

    loading api call

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,56 @@
+import logo from "../../assets/logo.webp";
+
+//  determining user salary range
+function formatSalaryRange(salary: number): string {
+  if (salary > 100) {
+    return "1+ CR";
+  } else {
+    const min = Math.floor(salary / 10) * 10;
+    const max = min + 10;
+    return `${min} to ${max} LPA`;
+  }
+}
+
+function Card({ role, location, salary, details, link, expYear }: any) {
+  const formattedSalary = formatSalaryRange(salary);
+
+  return (
+    <div className="w-full md:w-1/3 p-4">
+      <div className="border rounded-3xl p-6 shadow-md ">
+        <div className="border w-max shadow-md p-2 text-xs rounded-full">
+          ⏳ Posted 10 days ago
+        </div>
+        <div className="my-4 flex">
+          <div>
+            <img src={logo} className="w-10 rounded-full" alt="Company Logo" />
+          </div>
+          <div className="ml-3">
+            <div className="text-gray-500 text-sm">WeekDay</div>
+            <div>{role}</div>
+            <div className="text-xs">{location}</div>
+          </div>
+        </div>
+        <div className="text-sm text-gray-600 font-extralight">
+          Estimated Salary:{" "}
+          <span className="font-medium">₹{formattedSalary}</span> ✅
+        </div>
+        <div>About Company:</div>
+        <div className="font-bold text-sm">About us</div>
+        <div>
+          <div className="text-sm font-extralight">{details}</div>
+          <a href={link} target="_blank">
+            <div className="flex justify-center text-[#4943db] cursor-pointer">
+              View Job
+            </div>
+          </a>
+        </div>
+        <div className="text-sm font-medium pt-4 text-gray-500">
+            Minimum Experience
+        </div>
+        <div>{expYear}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,52 @@
 import { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 import axios from "axios";
+import Card from "../components/ui/Card";
+import { RotateCcwIcon } from "lucide-react";
+
 
 function Home() {
-  const [data, setData] = useState("");
+  
+  const [show, setShow] = useState(10)
+  const [data, setData] = useState<any[]>([]);
   useEffect(() => {
     axios
       .post("https://api.weekday.technology/adhoc/getSampleJdJSON", {
-        limit: 1,
+        limit: show,
         offset: 0,
       })
       .then((res) => setData(res.data.jdList));
-  }, []);
+  }, [show]);
   console.log(data);
-  return (
-    <div className="h-full ">
-      <SearchBar />
 
-      {JSON.stringify(data)}
+  function handleIncrease(){
+    setShow(show+10)
+  }
+
+ 
+
+  return (
+    <div className="h-full w-full ">
+      {/* Searchbar component */}
+      <SearchBar />
+      <div className="flex flex-wrap px-4 pt-10">
+        {/* Mapping over api data */}
+        {data.map((job, index) => (
+          <Card
+            key={index}
+            role={job.jobRole}
+            location={job.location}
+            salary={job.maxJdSalary}
+            details={job.jobDetailsFromCompany}
+            link={job.jdLink}
+            expYear={job.minExp || 0}
+          />
+        ))}
+      </div>
+
+      <div onClick={handleIncrease} className="flex items-center justify-center font-bold cursor-pointer text-xl py-4 mb-8">
+        <RotateCcwIcon className="mr-4" /> Load More
+      </div>
     </div>
   );
 }
